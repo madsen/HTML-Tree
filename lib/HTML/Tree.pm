2f044fe663537f08e7787d4c187e25fcdb8bb1b1
Jeff Fearn <jfearn@captcha.(none)>
2010-06-21 14:19:08 +1000

    correct developer release version number

--- lib/HTML/Tree.pm
+++ lib/HTML/Tree.pm
@@ -6,7 +6,7 @@ HTML::Tree - build and scan parse-trees of HTML
 
 =head1 VERSION
 
-3.24
+3.23_1
 
 =cut
 
@@ -18,7 +18,7 @@ use strict;
 use HTML::TreeBuilder ();
 
 use vars qw( $VERSION );
-$VERSION = '3.24_1';
+$VERSION = '3.23_1';
 
 =head1 SYNOPSIS
 
