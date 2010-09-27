madsen/HTML-Tree

commit 029b3569c198a8ba96834f62bd37675c32b82c4b

move attribute name validation to as_XML. RT #60619 bump to _4

lib/HTML/Element.pm
@@ -6,7 +6,7 @@ HTML::Element - Class for objects that represent HTML elements
 
 =head1 VERSION
 
-Version 3.23_3
+Version 3.23_4
 
 =cut
 
@@ -17,7 +17,7 @@ use HTML::Tagset   ();
 use integer;    # vroom vroom!
 
 use vars qw( $VERSION );
-$VERSION = '3.23_3';
+$VERSION = '3.23_4';
 
 # This contorls encoding entities on output.
 # When set entities won't be re-encoded.
@@ -270,10 +270,6 @@ sub new {
     my $self = bless { _tag => scalar( $class->_fold_case($tag) ) }, $class;
     my ( $attr, $val );
     while ( ( $attr, $val ) = splice( @_, 0, 2 ) ) {
-
-        # '/' means you've hit the end of a short '/>' tag
-        Carp::croak("$tag has an invalid attribute name '$attr' ' $val'")
-            unless ( $attr eq '/' || $self->_valid_name($attr) );
         $val = $attr unless defined $val;
         $self->{ $class->_fold_case($attr) } = $val;
     }
@@ -1812,6 +1808,13 @@ sub as_XML {
             if ( ref $node ) {     # it's an element
                 $tag = $node->{'_tag'};
                 if ($start) {      # on the way in
+
+                    foreach my $attr ( $node->all_attr_names() ) {
+                        Carp::croak(
+                            "$tag has an invalid attribute name '$attr'")
+                            unless ( $self->_valid_name($attr) );
+                    }
+
                     if ( $empty_element_map->{$tag}
                         and !@{ $node->{'_content'} || $nillio } )
                     {
@@ -1821,19 +1824,19 @@ sub as_XML {
                         push( @xml, $node->starttag_XML(undef) );
                     }
                 }
-                else {             # on the way out
+                else {    # on the way out
                     unless ( $empty_element_map->{$tag}
                         and !@{ $node->{'_content'} || $nillio } )
                     {
                         push( @xml, $node->endtag_XML() );
-                    }    # otherwise it will have been an <... /> tag.
+                    }     # otherwise it will have been an <... /> tag.
                 }
             }
-            else {       # it's just text
+            else {        # it's just text
                 _xml_escape($node);
                 push( @xml, $node );
             }
-            1;           # keep traversing
+            1;            # keep traversing
         }
     );
 

t/attributes.t
@@ -15,7 +15,7 @@ use Test::More tests => 2;
 use HTML::TreeBuilder;
 
 my $tb = HTML::TreeBuilder->new();
-$tb->parse( "<a href='http://wherever/moo.cgi?xyz=123&lang=en'>Test</a>" );
+$tb->parse("<a href='http://wherever/moo.cgi?xyz=123&lang=en'>Test</a>");
 
 my @links = $tb->look_down( sub { $_[0]->tag eq "a" } );
 my $href = $links[0]->attr("href");
@@ -23,9 +23,9 @@ my $href = $links[0]->attr("href");
 ok( $href =~ /lang/, "href should contain 'lang' (is: $href)" );
 
 # invalid attribute names (RT 23439)
-eval {
-    my $html = HTML::TreeBuilder->new_from_content('<img inval!d="asd">');
-};
+my $html = HTML::TreeBuilder->new_from_content('<img inval!d="asd">');
+
+eval { $html->as_XML(); };
 
 like(
     $@,

lib/HTML/Tree.pm
@@ -6,7 +6,7 @@ HTML::Tree - build and scan parse-trees of HTML
 
 =head1 VERSION
 
-3.23_3
+3.23_4
 
 =cut
 
@@ -18,7 +18,7 @@ use strict;
 use HTML::TreeBuilder ();
 
 use vars qw( $VERSION );
-$VERSION = '3.23_3';
+$VERSION = '3.23_4';
 
 =head1 SYNOPSIS
 

lib/HTML/TreeBuilder.pm
@@ -5,7 +5,7 @@ use strict;
 use integer;    # vroom vroom!
 use Carp ();
 use vars qw(@ISA $VERSION $DEBUG);
-$VERSION = '3.23_3';
+$VERSION = '3.23_4';
 
 #---------------------------------------------------------------------------
 # Make a 'DEBUG' constant...

commit 6c40ccbcc5eae9dac6fe005b30c6bc1a8e7e8f47

XHTML's '/' is valid attribute name. This validation breaks web contents scrapers for XHTML.
It was broken from: http://github.com/jfearn/HTML-Tree/commit/029b3569c198a8ba96834f62bd37675c32b82c4b

lib/HTML/Element.pm
@@ -1826,7 +1826,7 @@ sub as_XML {
                     foreach my $attr ( $node->all_attr_names() ) {
                         Carp::croak(
                             "$tag has an invalid attribute name '$attr'")
-                            unless ( $self->_valid_name($attr) );
+                            unless ( $attr eq '/' || $self->_valid_name($attr) );
                     }
 
                     if ( $empty_element_map->{$tag}

t/attributes.t
@@ -11,7 +11,7 @@
 use warnings;
 use strict;
 
-use Test::More tests => 2;
+use Test::More tests => 3;
 use HTML::TreeBuilder;
 
 my $tb = HTML::TreeBuilder->new();
@@ -32,6 +32,12 @@ like(
     qr|img has an invalid attribute name 'inval!d'|,
     'catch invalid atribute names'
 );
+
+# xhtml
+my $xhtml = HTML::TreeBuilder->new_from_content(q{<img src="foo.gif" />});
+my $img = $xhtml->find_by_tag_name('img');
+like($img->as_XML(), qr{<img src="foo\.gif" />});
+$xhtml = $xhtml->delete;
 
 exit;
 